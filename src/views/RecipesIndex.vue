e4495fdc64b4a91ca6cb64ed9ec8ecdd45db5c11
robocop019 <chrisbowen@comcast.net>
2020-04-15 16:07:29 -0500

    added ascending/descending sort

--- src/views/RecipesIndex.vue
+++ src/views/RecipesIndex.vue
@@ -5,11 +5,16 @@
       <h1>All Recipes</h1>
 
       <div>
-        <button class='btn btn-info text-info bg-dark m-3' v-on:click="setSortAttribute('title')">Sort by Title</button>
-        <button class='btn btn-info text-info bg-dark m-3' v-on:click="setSortAttribute('prep_time')">Sort by Prep Time</button>
+        <button class='btn btn-info text-info bg-dark m-3' v-on:click="setSortAttribute('title')">
+          Sort by Title <span v-if="sortAttribute === 'title'">{{sortArrow}}</span>
+        </button>
+
+        <button class='btn btn-info text-info bg-dark m-3' v-on:click="setSortAttribute('prep_time')">
+          Sort by Prep Time <span v-if="sortAttribute === 'prep_time'">{{sortArrow}}</span>
+        </button>
       </div>
 
-      <div v-for="recipe in orderBy(filterBy(recipes, $parent.titleFilter, 'title'), sortAttribute)">
+      <div v-for="recipe in orderBy(filterBy(recipes, $parent.titleFilter, 'title'), sortAttribute, sortAscending)">
         <h3><router-link :to="'/recipes/' + recipe.id"> {{ recipe.title }} </router-link></h3>
         <img :src="recipe.image_url" :alt="recipe.title">
       </div>
@@ -30,7 +35,9 @@
     data: function() {
       return {
         recipes: [],
-        sortAttribute: 'title'
+        sortAttribute: 'title',
+        sortAscending: 1,
+        sortArrow: '⬆️'
       };
     },
     created: function() {
@@ -40,6 +47,19 @@
     },
     methods: {
       setSortAttribute: function(inputAttribute) {
+
+        if (this.sortAttribute === inputAttribute) {
+          this.sortAscending *= -1;
+          if (this.sortArrow === '⬆️') {
+            this.sortArrow = '⬇️';
+          } else {
+            this.sortArrow = '⬆️';
+          }
+        } else {
+          this.sortAscending = 1;
+          this.sortArrow = '⬆️';
+        }
+
         this.sortAttribute = inputAttribute;
       }
     },
